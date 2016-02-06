26a049522a84ec32fc5c0a589dc10d0806dc791a
Haruhi <Haruhi>
2016-02-06 20:13:03 +0800

    For Academic User Only

--- src/main/java/FServlet.java
+++ src/main/java/FServlet.java
@@ -1,25 +1,53 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-
-@SuppressWarnings("serial")
+@WebServlet(name = "FServlet", urlPatterns = {"/FServlet"})
 public class FServlet extends HttpServlet {
 
-protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	System.out.println("Upload Servlet");
+ protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, Exception  {
+   	System.out.println("Upload Servlet");
     	
     	request.setAttribute("sample","This is the Servlet!");
     	
         response.setContentType("text/html");
         response.setStatus(200);
-        request.getRequestDispatcher("/src/main/webapp/index.jsp").forward(request, response);
+        response.sendRedirect("index.jsp");
     }
+	
+	@Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        try {
+            processRequest(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(FServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+	
+	@Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(FServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+	 @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
 }
