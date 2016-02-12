f95a4c84fc386c81baaf0e7a7f9cd9a5b7a5e007
Haruhi <Haruhi>
2016-02-12 17:12:49 +0800

    Updated IServlet.java

--- src/main/java/Servlet/FServlet.java
+++ src/main/java/Servlet/FServlet.java
@@ -94,25 +94,6 @@ public class FServlet extends HttpServlet {
 			JSONObject objcon = (JSONObject) parser.parse(concepts.toString());
 			request.setAttribute("concepts",objcon);
 			
-			//For keywords
-			JSONObject objkey = (JSONObject) parser.parse(keywords.toString());
-			request.setAttribute("keywords",objkey);
-			
-			//For entities
-			JSONObject objent = (JSONObject) parser.parse(entities.toString());
-			request.setAttribute("entities",objent);
-			
-			//For feeds
-			JSONObject objfeeds = (JSONObject) parser.parse(feeds.toString());
-			request.setAttribute("feeds",objfeeds);
-			
-			//For sentiment
-			JSONObject objsent = (JSONObject) parser.parse(sentiment.toString());
-			request.setAttribute("sentiment",objsent);
-			
-			//For relations
-			JSONObject objrel = (JSONObject) parser.parse(relations.toString());
-			request.setAttribute("relations",objrel);
 
 		} catch (Exception e) {
 			e.printStackTrace(System.err);
