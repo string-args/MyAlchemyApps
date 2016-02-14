f366ea0c0c6a42ddd3abd2fd653af439053e78a1
Haruhi <Haruhi>
2016-02-14 09:22:12 +0800

    Updated FServlet.java

--- src/main/java/Servlet/FServlet.java
+++ src/main/java/Servlet/FServlet.java
@@ -23,100 +23,45 @@ import org.json.simple.parser.JSONParser;
 @WebServlet(name = "FServlet", urlPatterns = {"/FServlet"})
 public class FServlet extends HttpServlet {
 
+
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException, Exception{
+		response.setContentType("text/html;charset=UTF-8");
+		
+		AlchemyConnector connector = new AlchemyConnector();
+		AlchemyLanguage al_service = new AlchemyLanguage();
+		al_service.setApiKey(connector.getAPIKey());
+		
+		String url = (String) request.getAttribute("furl");
+		
+		Map<String,Object> params = new HashMap<String,Object>();
+		params.put(AlchemyLanguage.URL,url);
+		
+		DocumentSentiment sentiment = al_service.getSentiment(params);
+		request.setAttribute("sentiment",sentiment);
+		response.sendRedirect("index.jsp");
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException{
+		try{
+			processRequest(request, response);
+		} catch (Exception ex){}
+	}
+	
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        AlchemyConnector connector = new AlchemyConnector();
-        
-        AlchemyLanguage service = new AlchemyLanguage();
-        service.setApiKey(connector.getAPIKey());
-		
-		//get url inputted by user
-		String furl = request.getParameter("furl");
-		
-		Map<String,Object> params = new HashMap<String,Object>();
-        params.put(AlchemyLanguage.URL, furl);
-        //For title
-		DocumentTitle title = service.getTitle(params);
-		//For language
-		Language language = service.getLanguage(params);
-		//For text
-        //DocumentText text = service.getText(params);
-		//For authors
-		DocumentAuthors authors = service.getAuthors(params);
-		//For taxonomy
-		Taxonomies taxonomy = service.getTaxonomy(params);
-		//For concepts
-		Concepts concepts = service.getConcepts(params);
-		/*
-		//For keywords
-		Keywords keywords = service.getKeywords(params);
-		//For entities
-		Entities entities = service.getEntities(params);
-		//For feeds
-		Feeds feeds = service.getFeeds(params);
-		//For sentiment
-		DocumentSentiment sentiment = service.getSentiment(params);
-		//For relations
-		SAORelations relations = service.getRelations(params);
-		*/
 		try{
-			JSONParser parser = new JSONParser();
-			//for title
-			JSONObject objtitle = (JSONObject) parser.parse(title.toString());
-			String parseTitle = (String) objtitle.get("title");
-			request.setAttribute("title", parseTitle);
+			processRequest(request, response);
+		} catch (Exception ex){}
 			
-			//For language
-			JSONObject objlang = (JSONObject) parser.parse(language.toString());
-			String parseLang = (String) objlang.get("language");
-			request.setAttribute("language", parseLang);
-			
-			//For text
-			//JSONObject objtext = (JSONObject) parser.parse(text.toString());
-			//String parseText = (String) objtext.get("text");
-			//request.setAttribute("text", parseText);
-			
-			//For authors
-			request.setAttribute("authors",authors);
-			
-			//For taxonomy
-			JSONObject objtax = (JSONObject) parser.parse(taxonomy.toString());
-			JSONArray arrtax = (JSONArray) objtax.get("taxonomy");
-			request.setAttribute("taxonomy", arrtax);
-			
-			//For concepts
-			JSONObject objcon = (JSONObject) parser.parse(concepts.toString());
-			request.setAttribute("concepts",objcon);
-		/*	
-			//For keywords
-			JSONObject objkey = (JSONObject) parser.parse(keywords.toString());
-			request.setAttribute("keywords",objkey);
-			
-			//For entities
-			JSONObject objent = (JSONObject) parser.parse(entities.toString());
-			request.setAttribute("entities",objent);
-			
-			//For feeds
-			JSONObject objfeeds = (JSONObject) parser.parse(feeds.toString());
-			request.setAttribute("feeds",objfeeds);
-			
-			//For sentiment
-			JSONObject objsent = (JSONObject) parser.parse(sentiment.toString());
-			request.setAttribute("sentiment",objsent);
-			
-			//For relations
-			JSONObject objrel = (JSONObject) parser.parse(relations.toString());
-			request.setAttribute("relations",objrel);
-*/
-		} catch (Exception e) {
-			e.printStackTrace(System.err);
-		}
-        response.setContentType("text/html");
-        response.setStatus(200);
-        request.getRequestDispatcher("index.jsp").forward(request, response);
-
     }
+	
+	@Override
+	public String getServletInfo(){
+		return "Short Description";
+	}
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,9 +1,7 @@
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
-<%@ page import="java.util.ArrayList" %>
-<%@ page import="java.util.HashMap" %>
-<%@ page import="java.util.List" %>
-<%@ page import="java.util.Map" %>
+<%@ page import="Bean.AlchemyConnector" %>
+
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -22,45 +20,20 @@
                         <input type="text" name="furl" size="80">
                         <input type="submit" value="Extract">
                     </form> <br/>
+					
 					<%
-                      if (request.getAttribute("title") != null){
-						out.println("<h4> Title: </h4>" + request.getAttribute("title"));
-						
-					  }
-					  if (request.getAttribute("language") != null){
-						out.println("<h4> Language: </h4>"+ request.getAttribute("language"));
-					  }
-					  if (request.getAttribute("authors") != null){
-						out.println("<h4> Authors: </h4>" + request.getAttribute("authors"));
-					  }
-					  if (request.getAttribute("text") != null){
-                        out.println("<h4>Text:</h4>");
-					    out.println("<p style = 'font-size: 10px;'> " + request.getAttribute("text") + "</p>");
-					  }
-					  if (request.getAttribute("taxonomy") != null){
-						out.println("<h4> Taxonomy </h4>");
-						out.println("<p style = 'font-size: 10px;'>" + request.getAttribute("taxonomy") + "</p>");
-					  } 
-					  if (request.getAttribute("concepts") != null){
-						out.println("<h4> Concepts: </h4>");
-						out.println("<p style = 'font-size: 10px;'>" + request.getAttribute("concepts") + "</p>");
-					  }
-                    %>
+						if (request.getAttribute("sentiment") != null){
+							out.println("<h3>" + request.getAttribute("sentiment") + "</h3>");
+						}
+					%>
+
                 </div>
                 <div id="content2">
                     <form action="IServlet" method="POST">
                         <input type="text" name="gurl" value="http://aib.edu.au/blog/wp-content/uploads/2015/08/bill-gates-jpg.jpg" size="80">
                         <input type="submit" value="Extract">
                     </form> <br/>
-					<%
-					if (request.getAttribute("imagekeywords") != null){
-						out.println("<h1>" + request.getAttribute("imagekeywords") + "</h1>");
-					}
-					if (request.getAttribute("imagefaces") != null){
-						out.println("<h1>" + request.getAttribute("imagefaces") + "</h1>");
-					}
-					
-					%>
+
                 </div>
             </div>
 			 
