dd118e64db6f83545d3be44fea5da87e50bd7bba
Haruhi <Haruhi>
2016-02-17 18:40:17 +0800

    Updated FServlet.java

--- src/main/java/Servlet/FServlet.java
+++ src/main/java/Servlet/FServlet.java
@@ -2,23 +2,17 @@ package Servlet;
 
 import Bean.AlchemyConnector;
 import com.ibm.watson.developer_cloud.alchemy.v1.AlchemyLanguage;
-import com.ibm.watson.developer_cloud.alchemy.v1.model.*;
+import com.ibm.watson.developer_cloud.alchemy.v1.model.DocumentSentiment;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+
 
 @WebServlet(name = "FServlet", urlPatterns = {"/FServlet"})
 public class FServlet extends HttpServlet {
@@ -26,8 +20,6 @@ public class FServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-				response.setContentType("text/html;charset=UTF-8");
-		
 		AlchemyConnector connector = new AlchemyConnector();
 		AlchemyLanguage al_service = new AlchemyLanguage();
 		al_service.setApiKey(connector.getAPIKey());
@@ -35,10 +27,12 @@ public class FServlet extends HttpServlet {
 		String input = (String) request.getAttribute("furl");
 		
 		Map<String,Object> params = new HashMap<String,Object>();
-		params.put(AlchemyLanguage.TEXT,input);
+		params.put(AlchemyLanguage.URL,input);
 		
 		DocumentSentiment sentiment = al_service.getSentiment(params);
 		request.setAttribute("sentiment",sentiment);
+		
+		response.setContentType("text/html;charset=UTF-8");
 		response.setStatus(200);
 		request.getRequestDispatcher("index.jsp").forward(request,response);		
     }
